0f6994a7968a10612308084518a80f6ac214ac0f
Debapriyo Paul <debapriyopaul.dp@gmail.com>
2022-05-05 17:43:47 +0530

    test case update

---
+++ lib/ceaserTest.dart
@@ -0,0 +1,113 @@
+import 'dart:async';
+
+import 'package:consest/ceaser.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class CeaserTest extends StatefulWidget {
+  const CeaserTest({Key? key}) : super(key: key);
+
+  @override
+  State<CeaserTest> createState() => _CeaserTestState();
+}
+
+class _CeaserTestState extends State<CeaserTest> with Ceaser {
+  static int testCaseNumber = 1;
+  bool success = false;
+  final TextEditingController _textEditingController = TextEditingController();
+  int key = 0;
+  String encrypted = "";
+  @override
+  Widget build(BuildContext context) {
+    if (testCaseNumber == 1 && encrypted == "") {
+      key = 4;
+      encrypted = encryption("Hello", key)!;
+    } else if (testCaseNumber == 2) {
+    } else if (testCaseNumber == 3) {
+    } else if (testCaseNumber == 4) {
+    } else if (testCaseNumber == 5) {}
+    Timer(const Duration(seconds: 10), () {
+      if (success) {
+        setState(() {
+          testCaseNumber = testCaseNumber + 1;
+          success = false;
+        });
+      }
+    });
+    return SafeArea(
+      child: Scaffold(
+          bottomNavigationBar: BottomNavigationBar(
+            onTap: (index) {
+              if (index == 0) {
+                Navigator.pop(context);
+              }
+            },
+            items: const [
+              BottomNavigationBarItem(
+                label: "Encoder",
+                icon: FaIcon(FontAwesomeIcons.keycdn),
+              ),
+              BottomNavigationBarItem(
+                label: "TestCase",
+                icon: FaIcon(FontAwesomeIcons.clipboardQuestion),
+              ),
+            ],
+            currentIndex: 1,
+          ),
+          appBar: AppBar(
+              title: Text("Test Case " + testCaseNumber.toString() + "/5")),
+          body: Form(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                Text(
+                  "Key : " + key.toString(),
+                  overflow: TextOverflow.visible,
+                ),
+                Text("Encrypted Message : " + encrypted,
+                    overflow: TextOverflow.clip),
+                TextFormField(
+                  controller: _textEditingController,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: 'Enter Decrypted Message',
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: (success)
+                      ? null
+                      : () {
+                          setState(() {
+                            if (testCaseNumber == 1) {
+                              if (_textEditingController.value.text
+                                      .toUpperCase() ==
+                                  "Hello".toUpperCase()) {
+                                success = true;
+                              }
+                            } else if (testCaseNumber == 2) {
+                            } else if (testCaseNumber == 3) {
+                            } else if (testCaseNumber == 4) {
+                            } else if (testCaseNumber == 5) {}
+                          });
+                        },
+                  child: const Text(
+                    'Submit',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 25,
+                    ),
+                  ),
+                ),
+                FaIcon(
+                  (success) ? FontAwesomeIcons.check : FontAwesomeIcons.xmark,
+                  color: (success) ? Colors.green : Colors.red,
+                  size: 40.0,
+                ),
+              ],
+            ),
+          )),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'package:consest/ceaser.dart';
+import 'package:consest/ceaserTest.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 void main() => runApp(const MyApp());
 
@@ -15,13 +18,15 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: const Text(appTitle),
         ),
-        body: MyCustomForm(),
+        body: const MyCustomForm(),
       ),
     );
   }
 }
 
 class MyCustomForm extends StatefulWidget {
+  const MyCustomForm({Key? key}) : super(key: key);
+
   @override
   State<MyCustomForm> createState() => _MyCustomFormState();
 }
@@ -33,218 +38,96 @@ class _MyCustomFormState extends State<MyCustomForm> {
 
   TextEditingController myController2 = TextEditingController();
 
-  String encrypt = "";
-
-  void encryption(String? message, int key) {
-    //String? message;
-    //int key;
-    //String encrypt = "";
-    if (message != null) {
-      message = message.toUpperCase();
-      key = key % 26;
-      int code = 0;
-      for (String i in message.split("")) {
-        code = findNumber(i);
-        if (code > 26) {
-          encrypt = encrypt + i;
-        } else {
-          code = code + key;
-          if (code > 26) {
-            code = code - 26;
-          }
-          if (i.toLowerCase().compareTo(i) == 0) {
-            encrypt = encrypt + findEncrypt(code).toLowerCase();
-          } else {
-            encrypt = encrypt + findEncrypt(code);
-          }
-          code = 0;
-        }
-      }
-      //print(encrypt);
-      //myController2.value=encrypt;
-    }
-  }
-
-  String findEncrypt(int code) {
-    switch (code) {
-      case 1:
-        return "A";
-      case 2:
-        return "B";
-      case 3:
-        return "C";
-      case 4:
-        return "D";
-      case 5:
-        return "E";
-      case 6:
-        return "F";
-      case 7:
-        return "G";
-      case 8:
-        return "H";
-      case 9:
-        return "I";
-      case 10:
-        return "J";
-      case 11:
-        return "K";
-      case 12:
-        return "L";
-      case 13:
-        return "M";
-      case 14:
-        return "N";
-      case 15:
-        return "O";
-      case 16:
-        return "P";
-      case 17:
-        return "Q";
-      case 18:
-        return "R";
-      case 19:
-        return "S";
-      case 20:
-        return "T";
-      case 21:
-        return "U";
-      case 22:
-        return "V";
-      case 23:
-        return "W";
-      case 24:
-        return "X";
-      case 25:
-        return "Y";
-      case 26:
-        return "Z";
-      default:
-        return " ";
-    }
-  }
-
-  int findNumber(String letter) {
-    switch (letter) {
-      case "A":
-        return 1;
-      case "B":
-        return 2;
-      case "C":
-        return 3;
-      case "D":
-        return 4;
-      case "E":
-        return 5;
-      case "F":
-        return 6;
-      case "G":
-        return 7;
-      case "H":
-        return 8;
-      case "I":
-        return 9;
-      case "J":
-        return 10;
-      case "K":
-        return 11;
-      case "L":
-        return 12;
-      case "M":
-        return 13;
-      case "N":
-        return 14;
-      case "O":
-        return 15;
-      case "P":
-        return 16;
-      case "Q":
-        return 17;
-      case "R":
-        return 18;
-      case "S":
-        return 19;
-      case "T":
-        return 20;
-      case "U":
-        return 21;
-      case "V":
-        return 22;
-      case "W":
-        return 23;
-      case "X":
-        return 24;
-      case "Y":
-        return 25;
-      case "Z":
-        return 26;
-      default:
-        return 27;
-    }
-  }
+  String? encrypt = "";
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-        child: Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Padding(
-          padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-          child: TextField(
-            controller: myController,
-            decoration: InputDecoration(
-              border: OutlineInputBorder(),
-              hintText: 'Enter a Text',
-            ),
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: (index) {
+          if (index == 1) {
+            Navigator.push(context,
+                MaterialPageRoute(builder: (context) => const CeaserTest()));
+          }
+        },
+        items: const [
+          BottomNavigationBarItem(
+            label: "Encoder",
+            icon: FaIcon(FontAwesomeIcons.keycdn),
           ),
-        ),
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-          child: TextFormField(
-            controller: myController1,
-            decoration: const InputDecoration(
-              border: OutlineInputBorder(),
-              hintText: 'Enter key',
-            ),
+          BottomNavigationBarItem(
+            label: "TestCase",
+            icon: FaIcon(FontAwesomeIcons.clipboardQuestion),
           ),
-        ),
-        SizedBox(
-          height: 10,
-        ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(
-                onPressed: () {
-                  setState(() {
-                    encryption(myController.value.text,
-                        int.parse(myController1.value.text));
-                  });
-                },
-                child: const Text(
-                  'Encrypt',
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 25,
-                    fontWeight: FontWeight.bold,
+        ],
+        currentIndex: 0,
+      ),
+      body: SafeArea(
+        child: SingleChildScrollView(
+            child: Form(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+                child: TextFormField(
+                  controller: myController,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: 'Enter a Text',
                   ),
-                )),
-          ],
-        ),
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-          child: Text(encrypt),
-          /*TextFormField(
-            controller: myController2,
+                ),
+              ),
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+                child: TextFormField(
+                  controller: myController1,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: 'Enter key',
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                      onPressed: () {
+                        setState(() {
+                          encrypt = Ceaser().encryption(myController.value.text,
+                              int.parse(myController1.value.text));
+                        });
+                      },
+                      child: const Text(
+                        'Encrypt',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 25,
+                        ),
+                      )),
+                ],
+              ),
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+                child: Text(encrypt!),
+                /*TextFormField(
+                  controller: myController2,
 
-            decoration: const InputDecoration(
-              border: OutlineInputBorder(),
-              hintText: 'out put',
-            ),
-          ),*/
-        ),
-      ],
-    ));
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: 'out put',
+                  ),
+                ),*/
+              ),
+            ],
+          ),
+        )),
+      ),
+    );
   }
 }
